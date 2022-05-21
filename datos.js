d6a5e1460d9844ee5db7c6d3b285ff0fa6aa7637
Ostap Demcenco <ostap.demcenco@inacapmail.cl>
2022-05-20 22:35:37 -0400

    Primer commit

---
+++ datos.js
@@ -0,0 +1,54 @@
+let productos = [
+  {
+    nombre: "Pan de Molde Blanco",
+    precio: 3000,
+    img: "./img/molde_blanco.jpg",
+  },
+  {
+    nombre: "Pan de Model Integral",
+    precio: 3000,
+    img: "./img/molde_integral.jpg",
+  },
+  {
+    nombre: "Pan Ciabatta",
+    precio: 300,
+    img: "./img/ciabatta.jpg",
+  },
+  {
+    nombre: "Pan Brioche",
+    precio: 300,
+    img: "./img/brioche.jpg",
+  },
+  {
+    nombre: "Pan de Cebolla",
+    precio: 3500,
+    img: "./img/cebolla.jpg",
+  },
+  {
+    nombre: "Pizza Margarita",
+    descripcion: "Pizza con salsa de tomate casera, queso mozarrella y pesto",
+    precio: 6500,
+    img: "./img/margarita.jpg",
+  },
+  {
+    nombre: "Pizza Iberica",
+    descripcion:
+      "Pizza con salsa de tomate casera, queso mozarrella, jamon serrano y rucula",
+    precio: 7500,
+    img: "./img/iberica.jpg",
+  },
+
+  { nombre: "Torta Brownie Naranja", precio: 30000, img: "./img/brownie.jpg" },
+  {
+    nombre: "Torta Trufa Frambuesa",
+    precio: 20000,
+    img: "./img/frambuesa.jpg",
+  },
+  {
+    nombre: "Berlin de Crema PAstelera",
+    precio: 1000,
+    img: "./img/berlin.jpg",
+  },
+  { nombre: "Mermelada Mora", precio: 3500, img: "./img/mora.jpg" },
+  { nombre: "Mermelada Arandanos", precio: 3500, img: "./img/arandanos.jpg" },
+];
